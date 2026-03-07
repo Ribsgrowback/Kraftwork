d57573c62664df49f52355d1b9f7755ab5e8a445
Ribsgrowback <max47380@gmail.com>
2026-03-07 23:25:28 +0900

    baek

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,21 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+data = list(input().upper())
+words = list(set(data))
+counts=[]
+placeholder=0
+dupetoggle=False
+wordsave=""
+for i in words:
+    wordcount= data.count(i)
+
+    if wordcount>placeholder:
+        placeholder=wordcount
+        wordsave=i
+        dupetoggle=False
+    elif wordcount==placeholder:
+        dupetoggle= True
+if dupetoggle==False :
+    print(wordsave)
+elif dupetoggle==True:
+    print('?')
--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,13 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+number = int(input())
+for j in range(number):
+    data = input().split()
+
+    counter = int(data[0])
+    arr = data[1]
+
+    for char in arr:
+        if counter > 0 :
+            print(char * counter, end='')
+    print()
