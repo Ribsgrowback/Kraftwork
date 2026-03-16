6d3a26784e04a24d9e933d08f6dd195d360d0b62
Ribsgrowback <max47380@gmail.com>
2026-03-16 16:56:22 +0900

    baek

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,25 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+
+def solver(arr):
+    arr.sort()
+
+    two_sum = set()
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            two_sum.add(arr[i] + arr[j])
+
+    for i in range(len(arr)-1, -1, -1):
+        d = arr[i]
+        for j in range(i+1):
+            z = arr[j]
+            if d-z in two_sum:
+                return d
+
+
+N = int(input())
+arr=[]
+for u in range(N):
+    arr.append(int(input()))
+
+print(solver(arr))
--- week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
+++ week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
@@ -1,2 +1,35 @@
 # 분할정복 - 색종이 만들기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/2630
+
+#arr = [list(map(int, input().split())) for _ in range(N)]
+
+def solve(x, y, size):
+    color = arr[x][y]
+    global white, blue
+    for i in range(x, x+size):
+        for j in range(y, y+size):
+            if arr[i][j] != color:
+                half = size // 2
+                solve(x, y, half)
+                solve(x, y+half, half)
+                solve(x+half, y, half)
+                solve(x+half, y+half, half)
+                return
+
+    if color == 0:
+        white += 1
+    else:
+        blue += 1
+
+
+N = int(input())
+arr = []
+
+for i in range(N):
+    arr.append(list(map(int, input().split())))
+white=0/4
+blue=0
+solve(0, 0, N)
+
+print(white)
+print(blue)
