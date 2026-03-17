a059be40a04b86f3aa2e27b6e60b78d59a73a495
Ribsgrowback <max47380@gmail.com>
2026-03-17 16:18:08 +0900

    잔디심기

--- week3/problem-solving/난이도중_링크드리스트_철도공사_골드4.py
+++ week3/problem-solving/난이도중_링크드리스트_철도공사_골드4.py
@@ -3,21 +3,62 @@
 
 # 음
 
-N, M= map(int, input).split()
+# 
 
-station=list(map(int,input).split())
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+station = list(map(int, input().split()))
+
+next={}
+prev={}
+
+for i in range(N):
+    next[station[i]]=station[(i+1) % N]
+    prev[station[i]]=station[(i-1) % N]
 
 for _ in range(M):
-    cmd=[]
-    # if cmd:
-    #     while(cmd):
-    #         cmd.pop()
-    
-    cmd=list(map(input).split())
+    cmd=input().split()
+
     if cmd[0]=="BN":
-        
-    elif cmd[0]=="BP":
+        i=int(cmd[1])
+        j=int(cmd[2])
 
-    elif cmd[0]=="CP":
+        print(next[i])
 
-    elif cmd[0]=="CN":
+        j_next = next[i]
+        next[i]=j
+        prev[j]=i
+        next[j]=j_next
+        prev[j_next]=j
+
+    elif cmd[0] == "BP":
+        i=int(cmd[1])
+        j=int(cmd[2])
+
+        print(prev[i])
+
+        i_prev = prev[i]
+        next[i_prev] = j
+        prev[j] = i_prev
+        next[j] = i
+        prev[i] = j
+
+    elif cmd[0] == "CN":
+        i = int(cmd[1])
+
+        target = next[i]
+        print(target)
+
+        next[i] = next[target]
+        prev[next[target]] = i
+
+    elif cmd[0] == "CP":
+        i = int(cmd[1])
+
+        target = prev[i]
+        print(target)
+
+        prev[i] = prev[target]
+        next[prev[target]] = i
