e8d3fd9b3c7f0f2544c93e795739fb3d73f484f6
Ribsgrowback <max47380@gmail.com>
2026-03-17 23:40:22 +0900

    prob

--- week3/problem-solving/난이도중_스택_탑_골드5.py
+++ week3/problem-solving/난이도중_스택_탑_골드5.py
@@ -1,2 +1,25 @@
 # 스택 - 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2493
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+heights = list(map(int, input().split()))
+
+stack = []   # (인덱스, 높이)
+answer = []
+
+for i in range(N):
+    h = heights[i]
+
+    while stack and stack[-1][1] < h:
+        stack.pop()
+
+    if not stack:
+        answer.append(0)
+    else:
+        answer.append(stack[-1][0])
+
+    stack.append((i + 1, h))  # 문제는 1번 탑부터 시작
+
+print(*answer)
--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,7 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+
+
+N= int(input())
+cmds = [input().split() for _ in range(N)]
+
