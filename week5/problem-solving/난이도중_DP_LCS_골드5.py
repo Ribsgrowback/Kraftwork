f35b4df1549f7bb42079851a827d4549a1a5829d
Ribsgrowback <max47380@gmail.com>
2026-03-30 19:38:18 +0900

    mid

--- week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -1,2 +1,14 @@
 # DP - LCS (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9251
+
+str1 = input()
+str2 = input()
+
+dp = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
+
+for i in range(1, len(str1) + 1):
+    for j in range(1, len(str2) + 1):
+        if str1[i - 1] == str2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,18 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+
+T = int(input())   #테스트 케이스 개수 입력받기
+
+for _ in range(T): 
+    N = int(input()) #동전가지수
+    coins = list(map(int, input().split())) #동전 종류 
+    M = int(input())            #금액 M
+
+    dp = [0] * (M + 1)  #배열 선언
+    dp[0] = 1
+
+    for coin in coins:
+        for j in range(coin, M + 1):
+            dp[j] = dp[j] + dp[j - coin]
+
+    print(dp[M])
