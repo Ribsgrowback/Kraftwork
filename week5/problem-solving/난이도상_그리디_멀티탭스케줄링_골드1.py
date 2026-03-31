1771bb19a4a159c10f5c269a7bc375f978532552
Ribsgrowback <max47380@gmail.com>
2026-03-31 13:26:50 +0900

    1234

--- week5/problem-solving/난이도상_그리디_멀티탭스케줄링_골드1.py
+++ week5/problem-solving/난이도상_그리디_멀티탭스케줄링_골드1.py
@@ -1,2 +1,37 @@
 # 그리디 - 멀티탭 스케줄링 (백준 골드1)
 # 문제 링크: https://www.acmicpc.net/problem/1700
+
+N, K = map(int, input().split())
+schedule = list(map(int, input().split()))
+
+multitap = []
+unplug_count = 0
+
+for i in range(K):
+    device = schedule[i]
+    if device in multitap:
+        continue
+    if len(multitap) < N:
+        multitap.append(device)
+        continue
+
+    unplug_count += 1
+    willPlug = {}
+    for j in range(i + 1, K):
+        if schedule[j] in multitap and schedule[j] not in willPlug:
+            willPlug[schedule[j]] = j
+
+    unpluged = None
+    future = -1
+    for d in multitap:
+        if d not in willPlug:
+            unpluged = d
+            break
+        elif willPlug[d] > future:
+            future = willPlug[d]
+            unpluged = d
+
+    multitap.remove(unpluged)
+    multitap.append(device)
+
+print(unplug_count)
--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,2 +1,21 @@
 # 그리디 - 신입 사원 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1946
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    applicants = []
+    for _ in range(N):
+        rank1, rank2 = map(int, input().split())
+        applicants.append((rank1, rank2)) 
+
+    applicants.sort(key=lambda x: x[0])
+
+    count = 1
+    best_rank2 = applicants[0][1] 
+    for i in range(1, N):
+        if applicants[i][1] < best_rank2: 
+            count += 1
+            best_rank2 = applicants[i][1] 
+
+    print(count)
